b8fb0573aaf6d8f5ae25d49998a2400c3409bbe5
Nathan <pitmannathan@gmail.com>
2014-10-04 17:36:30 +1300

    Added Do examples

---
+++ examples/ObjectExt/DoExamples.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Fluency.Extensions.ObjectExt
+{
+    
+    static class DoExamples
+    {
+        internal static void Do()
+        {
+            var strings = new[] { "aaa", "bbb", "cccc" };
+
+            WithoutDo(strings);
+            WithDo(strings);
+        }
+
+
+        static int WithoutDo(IEnumerable<string> input)
+        {
+            var sum = input
+                .Select(i => i + i)
+                .Sum(str => str.Length);
+
+            Console.WriteLine(sum);
+            
+            return Math.Max(sum, 7);
+        }
+
+
+        static int WithDo(IEnumerable<string> input)
+        {
+            return input
+                .Select(i => i + i)
+                .Sum(str => str.Length)
+                .Do(Console.WriteLine)
+                .Pipe(i => Math.Max(i, 7));
+        }
+    }
+
+}
